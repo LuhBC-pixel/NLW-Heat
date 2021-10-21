40d3740ccfa1c75455e4ea87fd1026313cae9011
LuhBC-pixel <bc.luisa@yahoo.com.br>
2021-10-21 11:40:02 -0300

    end

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,8 +1,8 @@
-import { api } from '../../services/api';
-import styles from './styles.module.scss';
-
-import logoImg from '../../assets/logo.svg';
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { api } from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+import styles from './styles.module.scss';
 
 type Message = {
   id: string;
@@ -13,8 +13,28 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then((response) => {
@@ -30,15 +50,10 @@ export function MessageList() {
         {messages.map((message) => {
           return (
             <li key={message.id} className={styles.message}>
-              <p className={styles.messageContent}>
-                {message.text}
-              </p>
+              <p className={styles.messageContent}>{message.text}</p>
               <div className={styles.messageUser}>
                 <div className={styles.userImage}>
-                  <img
-                    src={message.user.avatar_url}
-                    alt={message.user.name}
-                  />
+                  <img src={message.user.avatar_url} alt={message.user.name} />
                 </div>
                 <span>{message.user.name}</span>
               </div>
